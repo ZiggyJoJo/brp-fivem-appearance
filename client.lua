666a2ad075e966585ff837653971c763772f732d
0pax <64065031+0pax@users.noreply.github.com>
2021-11-25 21:16:45 +0100

    Update client.lua
    Fix for esx legacy ped caching, add check for esx legacy config as well as some small changes such as not allowing ped change in barbershop & clotheshop.

--- client.lua
+++ client.lua
@@ -150,7 +150,7 @@ Citizen.CreateThread(function()
 				if CurrentAction == 'barberMenu' then
 
 					local config = {
-						ped = true,
+						ped = false,
 						headBlend = true,
 						faceFeatures = true,
 						headOverlays = true,
@@ -161,9 +161,13 @@ Citizen.CreateThread(function()
 					exports['fivem-appearance']:startPlayerCustomization(function (appearance)
 						if (appearance) then
 							TriggerServerEvent('fivem-appearance:save', appearance)
-							print('Saved')
+							if Config.UseLegacy then
+								ESX.SetPlayerData('ped', PlayerPedId()) -- Fix for esx legacy
+							end
 						else
-							print('Canceled')
+							if Config.UseLegacy then
+								ESX.SetPlayerData('ped', PlayerPedId()) -- Fix for esx legacy
+							end
 						end
 					end, config)
 				end
@@ -219,7 +223,7 @@ end)
 
 RegisterNetEvent('fivem-appearance:clothingMenu', function()
 	local config = {
-		ped = true,
+		ped = false,
 		headBlend = false,
 		faceFeatures = false,
 		headOverlays = false,
@@ -230,9 +234,13 @@ RegisterNetEvent('fivem-appearance:clothingMenu', function()
 	exports['fivem-appearance']:startPlayerCustomization(function (appearance)
 		if (appearance) then
 			TriggerServerEvent('fivem-appearance:save', appearance)
-			print('Saved')
+			if Config.UseLegacy then
+				ESX.SetPlayerData('ped', PlayerPedId()) -- Fix for esx legacy
+			end
 		else
-			print('Canceled')
+			if Config.UseLegacy then
+				ESX.SetPlayerData('ped', PlayerPedId()) -- Fix for esx legacy
+			end
 		end
 	end, config)
 end)
@@ -300,11 +308,17 @@ AddEventHandler('fivem-appearance:setOutfit', function(data)
 		exports['fivem-appearance']:setPedProps(playerPed, pedProps)
 		local appearance = exports['fivem-appearance']:getPedAppearance(playerPed)
 		TriggerServerEvent('fivem-appearance:save', appearance)
+		if Config.UseLegacy then
+			ESX.SetPlayerData('ped', PlayerPedId()) -- Fix for esx legacy
+		end
 	else
 		exports['fivem-appearance']:setPedComponents(playerPed, pedComponents)
 		exports['fivem-appearance']:setPedProps(playerPed, pedProps)
 		local appearance = exports['fivem-appearance']:getPedAppearance(playerPed)
 		TriggerServerEvent('fivem-appearance:save', appearance)
+		if Config.UseLegacy then
+			ESX.SetPlayerData('ped', PlayerPedId()) -- Fix for esx legacy
+		end
 	end
 end)
 
@@ -397,11 +411,15 @@ AddEventHandler('esx_skin:openSaveableMenu', function(submitCb, cancelCb)
 	exports['fivem-appearance']:startPlayerCustomization(function (appearance)
 		if (appearance) then
 			TriggerServerEvent('fivem-appearance:save', appearance)
-			print('Saved')
+			if Config.UseLegacy then
+			   ESX.SetPlayerData('ped', PlayerPedId()) -- Fix for esx legacy
+			end
 			if submitCb then submitCb() end
 		else
-			print('Canceled')
 			if cancelCb then cancelCb() end
+			if Config.UseLegacy then
+			   ESX.SetPlayerData('ped', PlayerPedId()) -- Fix for esx legacy
+			end
 		end
 	end, config)
 end)
